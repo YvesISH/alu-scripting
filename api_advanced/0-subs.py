6f6e6c2a90472a99f7dde9d107affd146c0d3303
YvesISH <y.ishimwe2@alustudent.com>
2023-03-06 03:41:40 -0800

    How many Subs

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhton3
+"""Return number of subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code = 200:
+        json_data = response.json()
+        subscriber_number = json_dataget('data').get(
+            'children')[0].get('data').get('subreddit_subscribers')
+        return subscriber_number
+    else:
+        return 0
