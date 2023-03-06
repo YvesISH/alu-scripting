36e43d649f4f18e10c6d7cb6913e2b6faf827016
YvesISH <y.ishimwe2@alustudent.com>
2023-03-06 03:47:08 -0800

    How many Subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Return number of subscribers for a given subreddit"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """The funct"""
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
